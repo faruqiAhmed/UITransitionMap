0d03797b55b3bf51930cad0af1bf20c98dda3e1c
Faruq <faruqdeveloper@gmail.com>
2021-09-19 13:35:12 +0600

    Initial Commit

---
+++ UITransitionMap/UITransitionMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITransitionMapApp.swift
+//  UITransitionMap
+//
+//  Created by Md Omar Faruq on 9/19/21.
+//
+
+import SwiftUI
+
+@main
+struct UITransitionMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
