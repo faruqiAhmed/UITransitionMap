3b1a7d9bc5271dacaf376448e3f34631d8e8255e
Faruq <faruqdeveloper@gmail.com>
2021-09-21 18:55:44 +0600

    Create an account

--- UITransitionMap/ContentView.swift
+++ UITransitionMap/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView_Previews: PreviewProvider {
 struct Home: View {
     var body: some View{
         VStack{
-            Login()
+           SingUp()
                
         }
     }
@@ -90,7 +90,89 @@ struct Login : View {
             })
             {
                 Spacer()
-                Text("アカウントをお持ちではありませんか？ -クレタ島とアカウント")
+                Text("アカウントをお持ちではありませんか？クレタ島とアカウント")
+                    .foregroundColor(.black)
+                    .padding(.vertical)
+                   
+            }
+           
+           
+            
+        }
+        .padding(.horizontal,25)
+       
+       
+       
+    }
+}
+struct SingUp : View {
+    @State var color = Color.black.opacity(0.7)
+     @State var name = ""
+    @State var email = ""
+    @State var pass = ""
+    @State var visible = false
+    var body: some View{
+        
+        VStack{
+             
+            Text(" クリティアカウント")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(self.color)
+                .padding(.top ,8)
+            TextField ("名前", text: self.$name)
+             
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 4).stroke(self.name != "" ?  Color("Color"): self.color,lineWidth: 2))
+            .padding(.top, 25)
+            
+                TextField ("Eメール", text: self.$email)
+                 
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ?  Color("Color"): self.color,lineWidth: 2))
+                .padding(.top, 25)
+            HStack (spacing: 15){
+                VStack{
+                    if self.visible{
+                        TextField("パスワード", text: self.$pass)
+                           
+                    } else{
+                        SecureField("パスワード", text: self.$pass)
+                        
+                    }
+                }
+                Button(action: {
+                    self.visible.toggle()
+                    
+                }) {
+                    
+                    Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                        .foregroundColor(Color.orange)
+                }
+            }
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 4).stroke(self.pass != "" ?  Color("Color"): self.color,lineWidth: 2))
+            .padding(.top, 25)
+            
+            Button(action:  {
+               
+            })
+            {
+               Text("クリティ島で")
+                .foregroundColor(.white)
+                .padding(.vertical)
+                .frame( width: UIScreen.main.bounds.width - 50)
+            }
+            .background(Color("Color"))
+            .cornerRadius(10)
+            .padding(.top, 25)
+            
+            Button (action:  {
+                
+            })
+            {
+                Spacer()
+                Text("すでに持っていて、アカウントを持っていますか？ ログイン")
                     .foregroundColor(.black)
                     .padding(.vertical)
                    
